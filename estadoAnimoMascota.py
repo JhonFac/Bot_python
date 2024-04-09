cd29c88ad45250c3f8979bf34afdf6c176993ea4
Jhon Bohorquez <jhon@btilab.co>
2024-04-08 21:43:37 -0500

    cors

--- estadoAnimoMascota.py
+++ estadoAnimoMascota.py
@@ -8,37 +8,36 @@ from flask_cors import CORS
 
 load_dotenv()
 
+
 # Configura el modelo y la API KEY
 GOOGLE_API_KEY = 'AIzaSyCQ-pNzRJ0SaE-zcvDFJZZ2Oj3UX_ClyiA'
 genai.configure(api_key=GOOGLE_API_KEY)
 modelo = genai.GenerativeModel('gemini-pro-vision')
 
 app = Flask(__name__)
-CORS(app) 
-@app.route('/', methods=['GET'])
-def index():
-    return 'hola'
+CORS(app, resources={r"/": {"origins": ""}}, supports_credentials=True)  # Configuración CORS para permitir cualquier origen
 
 @app.route('/analizar', methods=['POST'])
 def analizar_imagen():
-    # Asegúrate de que la imagen se envíe como 'file' en el formulario
-    if 'file' not in request.files:
-        return jsonify({'error': 'No se ha enviado ninguna imagen'}), 400
+    # Verificar si se ha enviado algún archivo en la solicitud
+    if 'imagen' not in request.files:
+        print(request.files)
+        return jsonify({'mensaje': 'No se ha enviado ninguna imagen'}), 200
     
-    imagen = request.files['file']
+    imagen = request.files['imagen']
     
-    # Comprueba si el archivo tiene un nombre y si es una imagen válida
+    # Comprobar si el archivo tiene un nombre y si es una imagen válida
     if imagen.filename == '' or not imagen.filename.endswith(('.jpg', '.jpeg', '.png')):
         return jsonify({'error': 'Archivo de imagen no válido'}), 400
     
-    # Abre la imagen con PIL
+    # Si se ha recibido una imagen, procesarla como lo hacías antes
     img = PIL.Image.open(imagen)
     
-    # Genera la respuesta del modelo
-    respuesta = modelo.generate_content(['Analiza la imagen y dime el estado de animo de la mascota, respuestas asi: Estado de Animo: ?, Tipo de Raza o nombre real: ? ,  como identificaste el estado de animo, se especifico y explicalo bien:?,   recomendaciones : ? ,  respuestas en español , entregame las respuestas siempre en un JSON ,', img], stream=True)
+    respuesta = modelo.generate_content(['Analiza la imagen y dime el estado de animo de la mascota, respuestas asi: Estado de Animo : ?, Tipo de Raza o nombre real: ? ,  como identificaste el estado de animo, se especifico y explicalo bien:?,   recomendaciones : ? ,  respuestas en español , entregame las respuestas siempre en un JSON ,', img], stream=True)
     respuesta.resolve()  # Esperar a que la generación se complete
-    respuesta = respuesta.text.replace('```json','').replace('```','')    
-    return (respuesta)  
+    respuesta = respuesta.text.replace('json','').replace('','')    
+    return (respuesta)
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
